41339baa8df873ba6650a0eb384b78ea94da8ce5
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-12-31 09:34:22 +0100

    added flight-app once again with latest angular version

--- flight-app/src/app/app.component.spec.ts
+++ flight-app/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'flight-app'`, () => {
+  it(`should have the 'flight-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('flight-app');
@@ -26,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('flight-app app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, flight-app');
   });
 });
