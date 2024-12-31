a39e6b28c03cfdd7b88547d35c4586fa626fb7af
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-12-31 09:48:53 +0100

    added http request to get flights

--- flight-app/src/app/flight-search/flight-search.component.ts
+++ flight-app/src/app/flight-search/flight-search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Flight } from '../flight';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-flight-search',
@@ -7,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrl: './flight-search.component.scss'
 })
 export class FlightSearchComponent {
+  from = 'Hamburg';
+  to = 'Graz';
+  flights: Array<Flight> = [];
+  selectedFlight: Flight | null = null;
+
+  /**
+   *
+   */
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+
+  }
+  
+
+  search(): void {
+    const url = 'http://demo.angulararchitects.io/api/flight';
+
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json');
+
+    const params = new HttpParams()
+      .set('from', this.from)
+      .set('to', this.to);
+
+    this.http.get<Flight[]>(url,  {headers, params}).subscribe({
+      next: (flights) => {
+        this.flights = flights;
+      },
+      error: (err) => {
+        console.error('Error', err);
+      }
+    })
+  }
+
+  select(f: Flight): void {
+    this.selectedFlight = f;
+  }
 
 }
